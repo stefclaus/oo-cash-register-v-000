4148964b4bb330ac9c4cdb4072fb7ed221647ef0
Stef Claus <stef.claus@gmail.com>
2018-10-05 01:40:06 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,9 +8,19 @@ class CashRegister
     @discount = discount
   end 
 
+
+  
   def add_item(title, price, quantity = 0)
-    @total = price + @total 
-    @quantity = quantity
+    if quantity > 1
+    @total = price*quantity + @total 
+    else 
+      @total = price + @total 
+    end 
+    
+  def apply_discount
+    @total = @total*((100-@discount))
+  end 
+
   end 
   
   
