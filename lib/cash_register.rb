5c71f475794260a417317a5bfee596cfbed72a96
Stef Claus <stef.claus@gmail.com>
2018-10-06 01:57:03 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
   
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :quantity
   
    def initialize(discount = nil)
     @total = total 
@@ -8,19 +8,26 @@ class CashRegister
     @discount = discount
   end 
 
-
   
   def add_item(title, price, quantity = 0)
     if quantity > 1
-    @total = price*quantity + @total 
+      @total = (price * quantity) + @total 
     else 
       @total = price + @total 
     end 
+    end 
     
   def apply_discount
-    @total = @total*((100-@discount))
+    @total = @total*(100-@discount)
   end 
 
+  def quantity
+    @quantity 
+  end 
+  
+  def add_item(title, price, quantity = 0)
+    @total = price + @total 
+    @quantity = quantity + @quantity
   end 
   
   
